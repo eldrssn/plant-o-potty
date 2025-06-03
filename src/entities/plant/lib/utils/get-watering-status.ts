5512c6726a0f4f89ec44be1afa055d5c25c0b7c6
maxkalimullin <maxkalimullin@gmail.com>
2025-06-03 18:54:33 +0500

    fix: datepicker validation

--- src/entities/plant/lib/utils/get-watering-status.ts
+++ src/entities/plant/lib/utils/get-watering-status.ts
@@ -1,19 +1,25 @@
 import { MIN_DAYS } from '../constants'
 import { pluralizeDays } from './pluralize-days'
 
-export const getWateringStatus = ({ date, difference }: { date: string | null; difference: number | null }) => {
+export const getWateringStatus = ({
+  date,
+  difference,
+}: {
+  date: string | null
+  difference: number | null
+}) => {
   if (date === null || difference === null) return
 
   if (difference === 0) {
     return 'Сегодня'
   }
 
-  if (difference > MIN_DAYS || difference < -MIN_DAYS) {
-    return `${date}`
-  }
-
   if (difference < 0) {
     return `Просрочен на  ${Math.abs(difference)} ${pluralizeDays(difference)}`
+  }
+
+  if (difference > MIN_DAYS || difference < -MIN_DAYS) {
+    return `${date}`
   }
 
   return `Через ${difference} ${pluralizeDays(difference)}`
--- src/features/plant/manage-plant/model/form-schema.ts
+++ src/features/plant/manage-plant/model/form-schema.ts
@@ -10,7 +10,17 @@ export const managePlantFormSchema = z.object({
     .int()
     .min(1)
     .max(75),
-  lastWateredDate: z.string({ invalid_type_error: 'Обязательное поле' }),
+  lastWateredDate: z.string({ invalid_type_error: 'Обязательное поле' }).refine(
+    (val) => {
+      const inputDate = new Date(val)
+      const today = new Date()
+      today.setHours(0, 0, 0, 0)
+      return inputDate <= today
+    },
+    {
+      message: 'Дата не может быть позже сегодняшнего дня',
+    }
+  ),
   notes: z.string().optional(),
   addedDate: z.string(),
 })

5eeec312b787c4851f266c0914a1e59ef37c99bd
maxkalimullin <maxkalimullin@gmail.com>
2025-06-03 19:37:02 +0500

    feature: watring plant

---
+++ src/features/plant/water-plant/lib/constants.ts
@@ -0,0 +1,1 @@
+export const TODAY_DATE_ISO = new Date().toISOString()
--- src/features/plant/water-plant/model/use-delete-plant-confirm.ts
+++ src/features/plant/water-plant/model/use-delete-plant-confirm.ts
@@ -1,15 +1,14 @@
 import { useParams } from 'react-router-dom'
 import { usePlantsStore } from 'entities'
+import { TODAY_DATE_ISO } from '../lib'
 
 export const useWaterPlantConfirm = (onClose: () => void) => {
   const { itemId } = useParams()
   const { waterPlant } = usePlantsStore()
 
-  const newWataringDateISO = new Date().toISOString()
-
   const handleDelete = () => {
     if (!itemId) return
-    waterPlant(itemId, newWataringDateISO)
+    waterPlant(itemId, TODAY_DATE_ISO)
     onClose()
   }
 
